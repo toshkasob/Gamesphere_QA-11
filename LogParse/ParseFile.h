e1679f598dbf46f85cbbf83bf60bb9dab669ae11
toshkasob <toshka_sob@mail.ru>
2021-11-01 01:59:13 +0300

    ignore old version, which is backup

--- LogParse/ParseFile.h
+++
@@ -1,7 +0,0 @@
-#ifndef H_PARSE_FILE
-#define H_PARSE_FILE
-#include <string>
-
-
-void parseFile(std::string& filename) ;
-#endif // !H_PARSE_FILE
--- LogParse/ParseInputArgs.h
+++
@@ -1,7 +0,0 @@
-﻿#ifndef H_PARSE_INPUT_ARGS
-#define H_PARSE_INPUT_ARGS
-
-void showInputArgs(char* exe_name) ;
-
-
-#endif // !H_PARSE_INPUT_ARGS
